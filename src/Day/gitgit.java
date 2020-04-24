f9a173179b9e864220667bb61939435e41c65afb
omerdemirtash <demirtas85omer@gmail.com>
2020-04-24 15:29:20 -0500

    -m "gitgit.java"

--- src/Day/gitgit.java
+++ src/Day/gitgit.java
@@ -2,6 +2,6 @@ package Day;
 
 public class gitgit {
     public static void main(String[] args) {
-        System.out.println("hi dad");
+        System.out.println("hi busta");
     }
 }
--- src/hulya.java
+++ src/hulya.java
@@ -1,1 +1,6 @@
 
+public class hulya {
+    public static void main(String[] args) {
+        System.out.println("hey mom");
+    }
+}

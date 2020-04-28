b7d4ebe752fceb024b55d6ad46a2449bbc3a43f1
omerdemirtash <demirtas85omer@gmail.com>
2020-04-28 14:28:30 -0500

    huly

--- src/hulya.java
+++ src/hulya.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
 
 public class hulya {
     public static void main(String[] args) {
+//<<<<<<< Updated upstream
         System.out.println("hey mom and dad");
+
+        int a= 350;
+
+        String b = ""+a;
+
+        String [] c= b.split("");
+
+        int total= 0;
+
+
+
+
+        }
+
     }
-}
+
+
